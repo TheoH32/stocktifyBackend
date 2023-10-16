bf66c3a4c4fc26584420ff77ca15a2d25a3b0738
rachit-j <rjaiswal.sd.77@gmail.com>
2023-10-16 12:19:44 -0700

    added stock api

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stonk/StockDataApiController.java
@@ -0,0 +1,34 @@
+package com.nighthawk.spring_portfolio.mvc.stonk;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+@RequestMapping(path = "/api")
+public class StockDataApiController {
+
+    @GetMapping("/stockdata")
+    public ResponseEntity<String> getStockData() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=compact&datatype=json"))
+                .header("X-RapidAPI-Key", "53ed50b3c5mshb1ebce663573fbap1a08a4jsneab1f395e0a6")
+                .header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            return new ResponseEntity<>(response.body(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error fetching stock data: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
