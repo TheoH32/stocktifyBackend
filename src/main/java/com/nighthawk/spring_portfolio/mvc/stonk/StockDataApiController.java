e8c137b40e9e5febed06f7ebcce87d7af3359472
gwang1224 <gracewang187@gmail.com>
2023-10-22 21:01:06 -0700

    alphavantage

--- src/main/java/com/nighthawk/spring_portfolio/mvc/stonk/StockDataApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stonk/StockDataApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping(path = "/api")
@@ -23,9 +24,8 @@ public class StockDataApiController {
     public ResponseEntity<String> getStockData() {
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://twelve-data1.p.rapidapi.com/stocks?exchange=NASDAQ&format=json"))
+                    .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=compact&datatype=json"))
                     .header("X-RapidAPI-Key", "53ed50b3c5mshb1ebce663573fbap1a08a4jsneab1f395e0a6")
-                    .header("X-RapidAPI-Host", "twelve-data1.p.rapidapi.com")
                     .method("GET", HttpRequest.BodyPublishers.noBody())
                     .build();
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
